79950b130dedca547ea268075c4232aaa7845e79
Rhys Baker <rhys.baker99@gmail.com>
2019-05-12 17:11:12 +0100

    added rgb sliders, bootstrap and logo

--- Collaborative Drawing/public/sketch.js
+++ Collaborative Drawing/public/sketch.js
@@ -1,26 +1,37 @@
 var socket;
-var r = (255, 0, 0);
-var g = (0, 255, 0);
-var b = (0, 0, 255);
+var rSlider, gSlider, bSlider;
+
 
 
 function setup() {
-    createCanvas(640, 480);
-    background(230);
+    var canvas = createCanvas(1000, 800);
+    canvas.parent('sketch-holder');
+    background(240);
 
     socket = io.connect('http://localhost:3000');
     socket.on("mouse", newDrawing);
+
+    rSlider = createSlider(0, 255, 255);
+    rSlider.position(20, 100);
+    gSlider = createSlider(0, 255, 0);
+    gSlider.position(20, 130);
+    bSlider = createSlider(0, 255, 0);
+    bSlider.position(20, 160);
+
 }
 
 
 function newDrawing(data) {
     noStroke();
-    fill(r, g, b);
+    fill(data.r,data.g,data.b);
     rect(data.x, data.y, 35, 35);
 }
 
 
 function mouseDragged() {
+    var r = rSlider.value();
+    var g = gSlider.value();
+    var b = bSlider.value();
     console.log("Sending: " + mouseX + "," + mouseY + " Colour: " + r + "," + g + "," + b)
 
     var data = {
@@ -34,6 +45,7 @@ function mouseDragged() {
     socket.emit("mouse", data)
 
     noStroke();
-    fill(255, 0, 0);
+    fill(r, g, b);
     rect(mouseX, mouseY, 30, 30);
 }
+
