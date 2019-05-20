dff4d4a2ab0b2cf3797a5b260c470ac4cec1547e
Rhys Baker <rhys.baker99@gmail.com>
2019-05-20 18:11:52 +0100

    update

--- Collaborative Drawing/config/dbconfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MongoURI: "mongodb+srv://user:admin@inde502-7sykr.mongodb.net/test?retryWrites=true"
-};
-
-
-// module.exports = {
-//   MongoURI: "mongodb://localhost:27017/users"
-// };
